ba5f73970e140b0795bf7e0064ab6181bef63733
Alex <adb1g18@soton.ac.uk>
2025-06-27 20:50:42 +0100

    Added initial code files. ble_investigator.py allows for investigating device characteristics and interaction with BLE device.

---
+++ ble_investigator.py
@@ -0,0 +1,261 @@
+import asyncio
+from bleak import BleakScanner
+from bleak import BleakClient
+import threading
+import struct
+
+
+'''
+TODO: 
+    Test write
+    Test multiple access properties
+    Alter the program to be interacted with in a terminal
+'''
+
+
+async def read(bleak_client, characteristic_uuid):
+    ble_data = await bleak_client.read_gatt_char(characteristic_uuid)
+    print("\nRead: {}\n".format(ble_data))
+    decoding_mode = input("Decode Type ( none / utf-8 / uint8_t / uint16_t / custom ) : ")
+    decoded_data = decode_data(ble_data, decoding_mode)
+    if "bytearray" not in str(decoded_data):
+        print("Decoded Data: {}\n".format(decoded_data))
+
+
+async def notify(bleak_client, characteristic_uuid):
+    decoding_mode = input("Decode Type ( none / utf-8 / uint8_t / uint16_t / custom ) : ")
+    unpacking_options = None
+    if decoding_mode == "custom":
+        unpacking_options = input("Set parameters to be used with struct.unpack(): ")
+
+    await bleak_client.start_notify(characteristic_uuid,
+                                    decode_notification_handler(decoding_mode, unpacking_options))
+    print("Listening for notifications. Press any key to stop...")
+    await nonblocking_wait_for_input()
+    await bleak_client.stop_notify(characteristic_uuid)
+    print("Unsubscribed from characteristic")
+
+
+async def write(bleak_client, characteristic_uuid):
+    byte_string = input("Input byte string of structure 0xXX, 0xXX, ... , 0xXX : ")
+    byte_strings = [b.strip() for b in byte_string.split(",")]
+    byte_values = []
+    not_byte_flag = False
+    for byte_string in byte_strings:
+        byte = is_byte(byte_string)
+        if byte:
+            byte_values.append(byte)
+        else:
+            not_byte_flag = True
+    if not_byte_flag is False:
+        await bleak_client.write_gatt_char(characteristic_uuid, bytearray(byte_values))
+        print("Write complete")
+    else:
+        print("Byte string was in an incorrect format")
+
+
+def get_characteristic_access_choice(characteristic_info):
+    characteristic_uuid = None
+    access_type = None
+    characteristic_choice = input("Access Characteristic Number: ")
+    if characteristic_choice.isnumeric():
+        characteristic_number = int(characteristic_choice)
+        characteristic_uuid = characteristic_info[characteristic_number][0]
+        access_types = characteristic_info[characteristic_number][1]
+        if len(access_types) <= 0:
+            print("Unknown Access Type")
+        elif len(access_types) == 1:
+            access_type = access_types[0]
+        else:
+            access_types_str = ""
+            for access_type in access_types:
+                if access_types.index(access_type) > 0:
+                    access_types_str += " / "
+                access_types_str += access_type
+
+            access_type = input("Choose from available access types ( {} ) : ".format(access_types_str))
+    return access_type, characteristic_uuid
+
+
+async def nonblocking_wait_for_input():
+    # Run input in another thread to avoid blocking the event loop
+    loop = asyncio.get_running_loop()
+    stop_event = asyncio.Event()
+
+    def wait_for_enter():
+        input()
+        loop.call_soon_threadsafe(stop_event.set)
+
+    threading.Thread(target=wait_for_enter, daemon=True).start()
+    await stop_event.wait()
+
+
+def is_byte(text):
+    try:
+        value = int(text, 16)
+        return 0 <= value <= 0xFF
+    except ValueError:
+        return False
+
+
+def decode_notification_handler(decoding_mode=None, custom_unpacking_options=None):
+    def notification_handler(sender, data):
+        if decoding_mode is not None:
+            data = decode_data(data, decoding_mode, custom_unpacking_options)
+        print("{}: {}".format("Notification", data))
+    return notification_handler
+
+
+def print_device_list(ble_devices_array):
+    print("-{:-^10}-{:-^26}-{:-^21}-".format("", "", ""))
+    print("|{:^10}|{:^26}|{:^21}|".format("Number", "Name", "UUID"))
+    print("-{:-^10}-{:-^26}-{:-^21}-".format("", "", ""))
+    for ble_device in ble_devices_array:
+        print("|{:^10}|{:^26}|{:^21}|".format(ble_devices_array.index(ble_device), ble_device[0], ble_device[1]))
+    print("-{:-^10}-{:-^26}-{:-^21}-".format("", "", ""))
+
+
+def strip_tuple(tuple_data):
+    if len(tuple_data) > 1:
+        str_data = str(tuple_data).strip("()")
+    else:
+        str_data = str(tuple_data).strip("(),")
+    return str_data
+
+
+def decode_data(coded_data, decoding_mode, passing_unpacking_options=None):
+    if decoding_mode == "utf-8":
+        decoded_data = coded_data.decode("utf-8")
+    elif decoding_mode == "uint8_t":
+        unpacked_data = struct.unpack('<' + 'B' * len(coded_data), coded_data)
+        decoded_data = strip_tuple(unpacked_data)
+    elif decoding_mode == "uint16_t":
+        unpacked_data = struct.unpack('<' + 'H' * (len(coded_data) // 2), coded_data)
+        decoded_data = strip_tuple(unpacked_data)
+    elif decoding_mode == "custom":
+        if passing_unpacking_options is None:
+            unpacking_options = input("Set parameters to be used with struct.unpack(): ")
+        else:
+            unpacking_options = passing_unpacking_options
+        try:
+            unpacked_data = struct.unpack(unpacking_options, coded_data)
+            decoded_data = strip_tuple(unpacked_data)
+        except Exception as e:
+            print("Invalid format setting that results in error: {}".format(e))
+            return coded_data
+    else:
+        return coded_data
+    return decoded_data
+
+
+async def connect(device_address, characteristic_info):
+    print("Connecting to device...")
+    async with (BleakClient(device_address) as bleak_client):
+        print("Connected to device")
+        while bleak_client.is_connected:
+            access_type, characteristic_uuid = get_characteristic_access_choice(characteristic_info)
+
+            if access_type == "read":
+                await read(bleak_client, characteristic_uuid)
+
+            elif access_type == "notify" or access_type == "indicate":
+                await notify(bleak_client, characteristic_uuid)
+
+            elif access_type == "write":
+                await write(bleak_client, characteristic_uuid)
+            else:
+                print("Invalid characteristic choice")
+
+            await asyncio.sleep(0.1)
+            disconnect_option = input("Disconnect from device? ( y / n ) : ")
+            if disconnect_option == "y":
+                await disconnect(bleak_client)
+
+
+async def disconnect(bleak_client):
+    if bleak_client.is_connected:
+        await bleak_client.disconnect()
+        print("Disconnected from device")
+
+
+async def device_scanner():
+    try:
+        print("Scanning devices...")
+        ble_devices = await BleakScanner.discover()
+        if ble_devices:
+            ble_devices_array = []
+            number = 0
+            for ble_device in ble_devices:
+                if ble_device.name:
+                    device_name = ble_device.name
+                else:
+                    device_name = "Unnamed"
+                ble_devices_array.append((device_name, ble_device.address))
+                number += 1
+            print_device_list(ble_devices_array)
+            return ble_devices_array
+
+    except Exception as e:
+        print("Bleak Scanner Failed - Possible Issue with Bluetooth Adapter")
+        exit("Error: {}".format(e))
+
+
+async def find_characteristics(device_address):
+    bleak_client = BleakClient(device_address)
+    print("Connecting to device...")
+    try:
+        await bleak_client.connect()
+        print("Connected to device")
+
+        print("-{:-^10}-{:-^18}-{:-^40}-{:-^29}-{:-^80}-".format("", "", "", "", ""))
+        print("|{:^10}|{:^18}|{:^40}|{:^29}|{:^80}|"
+              .format("Number", "Type", "UUID", "Description / Access", "Value"))
+        characteristic_info = []
+        number = 0
+        for service in bleak_client.services:
+            print("-{:-^10}-{:-^18}-{:-^40}-{:-^29}-{:-^80}-".format("", "", "", "", ""))
+            print("|{:^10}|{:^18}|{:^40}|{:^29}|{:^80}|"
+                  .format("", "Service", service.uuid, service.description, ""))
+            for char in service.characteristics:
+                char_properties_text = ", ".join(char.properties)
+                value = ""
+                if "read" in char.properties:
+                    try:
+                        value = await bleak_client.read_gatt_char(char.uuid)
+                        stripped = (str(value).replace("bytearray", "")
+                                              .replace("(", "").replace(")", ""))
+                        if "\\x" in str(value):
+                            value = stripped
+                        elif stripped.isascii():
+                            value = value.decode('utf-8')
+                    except Exception as e:
+                        value = "Read failed: {}".format(e)
+                characteristic_info.append((char.uuid, char.properties))
+                print("|{:^10}|{:^18}|{:^40}|{:^29}|{:^80}|"
+                      .format(number, "Characteristic", char.uuid, char_properties_text, value))
+                number += 1
+        print("-{:-^10}-{:-^18}-{:-^40}-{:-^29}-{:-^80}-".format("", "", "", "", ""))
+        await disconnect(bleak_client)
+        return characteristic_info
+
+    except Exception as e:
+        print("Connection terminated on BLE Device")
+        exit("Error: {}".format(e))
+
+
+if __name__ == "__main__":
+    characteristic = "FF:EE:DD:CC:BB:AA"
+    devices = asyncio.run(device_scanner())
+    selected_device = input("Connect to Device Number: ")
+    if selected_device.isdigit():
+        selected_device = int(selected_device)
+        if len(devices) > selected_device >= 0:
+            address = devices[selected_device][1]
+            characteristics = asyncio.run(find_characteristics(address))
+            access = input("Interact with device? ( y / n ) : ")
+            if access == "y":
+                asyncio.run(connect(address, characteristics))
+            else:
+                print("Program Exiting...")
+    else:
+        print("Invalid Input. Program Exiting...")
---
+++ multiprocessing_functions.py
@@ -0,0 +1,100 @@
+from bleak import BleakScanner
+from bleak import BleakClient
+import asyncio
+import multiprocessing
+import struct
+
+
+def decode_matrix_data(num_rows, num_cols, byte_array):
+    # Assuming each element is a 12-bit unsigned integer (2 bytes)
+    element_size = 2
+
+    # Unpack the byte array into a flat list of integers
+    unpacked_matrix_data = struct.unpack('<' + 'H' * (len(byte_array) // element_size), byte_array)
+    # Reshape the flat list into a 2D matrix
+    matrix_data = [unpacked_matrix_data[i:i+num_rows] for i in range(0, len(unpacked_matrix_data), num_cols)]
+
+    return matrix_data
+
+
+def decode_matrix_dimensions(byte_array):
+    num_of_rows, num_of_cols = struct.unpack('<BB', byte_array)
+    return num_of_cols, num_of_rows
+
+
+def start_asyncio(target, args):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    if isinstance(args, tuple) and len(args) > 0:
+        loop.run_until_complete(target(*args))
+    else:
+        loop.run_until_complete(target(args))
+    loop.close()
+
+
+def process_handler(target, args):
+    process = multiprocessing.Process(target=start_asyncio, args=(target, args))
+    process.start()
+    return process
+
+
+# Function to update the listbox with detected devices
+async def device_scanner(queue, lock, data_availability):
+    try:
+        devices = await BleakScanner.discover()
+        if devices:
+            devices_2d = []
+            for device in devices:
+                if device.name:
+                    device_name = device.name
+                else:
+                    device_name = "None"
+                devices_2d.append((device.address, device_name))
+            lock.acquire()
+            queue.put(devices_2d)
+            data_availability.value = 1
+            lock.release()
+            print("Devices Found")
+    except Exception as e:
+        print("Bleak Scanner Failed - Possible Issue with Bluetooth Adapter")
+        print("Error: {}".format(e))
+        data_availability.value = 2
+
+
+async def connect(queue, lock, connected, data_availability, device_address,
+                  dimensions_characteristic, data_characteristic):
+    client = BleakClient(device_address)
+    try:
+        await client.connect()
+        print("Connected to the device")
+        lock.acquire()
+        connected.value = 1
+        lock.release()
+        matrix_dimensions = await client.read_gatt_char(dimensions_characteristic)
+        number_of_rows, number_of_columns = decode_matrix_dimensions(matrix_dimensions)
+        lock.acquire()
+        queue.put(matrix_dimensions)
+        data_availability.value = 1
+        lock.release()
+        while connected.value:
+            byte_array = await client.read_gatt_char(data_characteristic)
+            matrix_data = decode_matrix_data(number_of_rows, number_of_columns, byte_array)
+            print(matrix_data)
+            matrix_data = [[0 for _ in range(number_of_columns)] for _ in range(number_of_rows)]
+            lock.acquire()
+            queue.put(matrix_data)
+            data_availability.value = 1
+            lock.release()
+
+    except Exception as e:
+        lock.acquire()
+        queue.put(None)
+        data_availability.value = 0
+        lock.release()
+        print("Connection terminated on BLE Device")
+        print("Error: {}".format(e))
+
+    finally:
+        if client.is_connected:
+            await client.disconnect()
+            print("Disconnected from the device")
